5e0b50611cd343d32926c5395afbb8ea654a433f
thnak <117495750+thnak@users.noreply.github.com>
2023-06-26 02:48:38 +0700

    Update decoder.py

--- slowfast/datasets/decoder.py
+++ slowfast/datasets/decoder.py
@@ -269,7 +269,7 @@ def torchvision_decode(
         decode_all_video (bool): if True, the entire video was decoded.
     """
     # Convert the bytes to a tensor.
-    video_tensor = torch.from_numpy(np.array(video_handle, dtype=np.uint8))
+    video_tensor = torch.from_numpy(np.frombuffer(np.array(video_handle), dtype=np.uint8))
 
     decode_all_video = True
     video_start_pts, video_end_pts = 0, -1
